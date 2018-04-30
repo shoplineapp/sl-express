d7ef152babca1f454da952c4c2351e23281661ff
Ted Cheng <tedcheng>
2018-04-30 12:22:45 +0800

    feat<example>: an new example of using Redis
    ;

---
+++ example/usingRedis/app.js
@@ -0,0 +1,22 @@
+let SuperApp = require('sl-express')
+
+class App extends SuperApp {
+
+  async connectDependencies() {
+
+    try { await super.connectDependencies() }catch(e) { throw e }
+    try { await this.connectRedis() }catch(e) { throw e }
+
+  }
+
+  async disconnectDependencies() {
+
+    try { await this.disconnectRedis() }catch(e) { throw e }
+    try { await super.disconnectDependencies() }catch(e) { throw e }
+
+  }
+
+}
+
+let app = new App
+module.exports = app

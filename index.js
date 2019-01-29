7848ba50360d380ec79617f00e5623921a8e6a20
Ted Cheng <tedcheng>
2019-01-29 10:53:59 +0800

    index will exports multiple models instead of just App

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-module.exports = require('./lib/models/App.js')
+module.exports = {
+  App: require('./lib/models/App.js'),
+  MongooseModel: require('./lib/models/MongooseModel.js'),
+  Router: require('./lib/models/Router.js'),
+  AppError: require('./lib/services/AppError.js'),
+  HandlerRouter: require('./lib/services/HandlerRouter.js'),
+  Logger: require('./lib/services/Logger.js'),
+  MessageQueue: require('./lib/services/MessageQueue.js'),
+  QueueTask: require('./lib/services/QueueTask.js'),
+  Redis: require('./lib/services/Redis.js'),
+}

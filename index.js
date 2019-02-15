740599adee3761ac73edae101b255d194756f514
Ted Cheng <tedcheng>
2019-02-15 18:02:45 +0800

    move logger and mongoose to plugin

--- index.js
+++ index.js
@@ -4,8 +4,5 @@ module.exports = {
   Router: require('./lib/models/Router.js'),
   AppError: require('./lib/services/AppError.js'),
   HandlerRouter: require('./lib/services/HandlerRouter.js'),
-  Logger: require('./lib/services/Logger.js'),
-//  MessageQueue: require('./lib/services/MessageQueue.js'),
   QueueTask: require('./lib/services/QueueTask.js'),
-//  Redis: require('./lib/services/Redis.js'),
 }

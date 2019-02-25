abc4cb3cd8a1fd4ff971716af2045acd115fad5b
Ted Cheng <tedcheng>
2019-02-25 13:07:42 +0800

    add backward compatible code

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ module.exports = {
   Router: require('./lib/models/Router.js'),
   AppError: require('./lib/services/AppError.js'),
   HandlerRouter: require('./lib/services/HandlerRouter.js'),
+  Logger: require('./lib/plugins/logger/lib/Logger.js'),
   QueueTask: require('./lib/services/QueueTask.js'),
+  MessageQueue: require('./lib/plugins/messageQueue/lib/RabbitMessageQueue.js'),
+  Redis: require('./lib/plugins/redis/lib/Redis.js'),
 }

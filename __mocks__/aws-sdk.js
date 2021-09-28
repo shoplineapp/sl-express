a142818b6d490c51736bf236a6d76f213e99aba8
Xin Li <aehrt55@aehrt55.net>
2021-09-28 11:07:28 +0800

    feat: use access key first

--- __mocks__/aws-sdk.js
+++ __mocks__/aws-sdk.js
@@ -1,9 +1,11 @@
 const SQS = require('./SQS.js')
+const Credentials = require('./Credentials')
 const TokenFileWebIdentityCredentials = require('./TokenFileWebIdentityCredentials')
 
 const AWS = jest.genMockFromModule()
 
 AWS.SQS = SQS
+AWS.Credentials = Credentials
 AWS.TokenFileWebIdentityCredentials = TokenFileWebIdentityCredentials
 
 module.exports = AWS
